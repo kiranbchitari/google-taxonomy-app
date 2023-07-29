3a13a60d4761ccf209a2e508e03168b448a54eb1
Kiran B Chitari <kiran.chitari@flexoffers.com>
2023-07-29 12:48:37 +0530

    UI enhancement

--- src/CategoryDetails.js
+++ src/CategoryDetails.js
@@ -3,26 +3,42 @@ import React from 'react';
 import { Paper, Typography, Grid } from '@mui/material';
 
 const CategoryDetails = ({ details }) => {
+  const labelStyle = { fontWeight: 'bold' };
+
   return (
-    <Paper elevation={3} style={{ padding: '20px', marginTop: '20px', maxWidth: '600px' }}>
-      <Typography variant="h5" gutterBottom>
+    <Paper elevation={3} style={{ padding: '20px', margin: '20px auto', maxWidth: '600px' }}>
+      <Typography variant="h5" gutterBottom align="center" style={labelStyle}>
         Category Details
       </Typography>
       <Grid container spacing={2}>
         <Grid item xs={6}>
-          <Typography variant="body1">Category ID: {details.id}</Typography>
+          <Typography variant="body1" style={labelStyle}>
+            Category ID:
+          </Typography>
         </Grid>
         <Grid item xs={6}>
-          <Typography variant="body1">Category Name: {details.name}</Typography>
+          <Typography variant="body1">{details.id}</Typography>
         </Grid>
-        <Grid item xs={12}>
-          <Typography variant="body1">Complete Path: {details.path.join(' > ')}</Typography>
+        <Grid item xs={6}>
+          <Typography variant="body1" style={labelStyle}>
+            Category Name:
+          </Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant="body1">{details.name}</Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant="body1" style={labelStyle}>
+            Complete Path:
+          </Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant="body1">{details.path.join(' > ')}</Typography>
         </Grid>
       </Grid>
       {/* Add more details as needed */}
-    </Paper>
+      </Paper>
   );
 };
 
 export default CategoryDetails;
-
